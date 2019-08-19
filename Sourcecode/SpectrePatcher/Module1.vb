894df940c2e12caf7481a1bd173804df4210e48b
Björn Kremer <bjoern.kremer1@gmail.com>
2019-08-19 14:33:36 +0200

    Refaktorisierung

--- Sourcecode/SpectrePatcher/Module1.vb
+++ Sourcecode/SpectrePatcher/Module1.vb
@@ -28,17 +28,15 @@ Module Module1
         Dim exePath As String = System.Reflection.Assembly.GetEntryAssembly().Location
         Dim workingDir As String = New System.IO.FileInfo(exePath).Directory.FullName
         Dim version As Version = Assembly.GetExecutingAssembly().GetName().Version
-        Dim p As New Process()
+
         Dim updateExe As String = IO.Path.Combine(workingDir, "SimpleAutoUpdate.NET.exe")
 
-        p.StartInfo.FileName = "cmd.exe"
-        p.StartInfo.Arguments = String.Format("/C """"{0}"" ""{1}"" ""{2}"" ""{3}"" 2>> ""{4}""""", updateExe, version.ToString(), "https://raw.githubusercontent.com/BkrBkr/SpectrePatcher/master/update.xml", exePath, logFile)
+        Dim fileName = "cmd.exe"
+        Dim arguments = String.Format("/C """"{0}"" ""{1}"" ""{2}"" ""{3}"" 2>> ""{4}""""", updateExe, version.ToString(), "https://raw.githubusercontent.com/BkrBkr/SpectrePatcher/master/update.xml", exePath, logFile)
+        Dim exitCode As Integer = SpectrePatcherHelper.StartProcess(fileName, arguments)
 
-        p.Start()
-        p.WaitForExit()
-
-        If p.ExitCode <> 0 Then
-            Throw New InvalidOperationException(String.Format("Error during auto update. Error-Code {0}", p.ExitCode))
+        If exitCode <> 0 Then
+            Throw New InvalidOperationException(String.Format("Error during auto update. Error-Code {0}", exitCode))
         End If
 
         If IO.File.Exists(IO.Path.Combine(workingDir, "SimpleAutoUpdate.NET.exe.update")) Then
@@ -49,167 +47,24 @@ Module Module1
 
     Sub Main()
         Dim logFile = System.Configuration.ConfigurationManager.AppSettings("logFile")
+        Dim downloadDir = System.Configuration.ConfigurationManager.AppSettings("downloadDir")
+
         Dim helper As New SpectrePatcherHelper
-        Dim doReboot As Boolean = False
         Try
-
             autoUpdate(logFile)
-            Dim downloadDir = System.Configuration.ConfigurationManager.AppSettings("downloadDir")
-            Try
-                If Not IO.Directory.Exists(downloadDir) Then
-                    IO.Directory.CreateDirectory(downloadDir)
-                End If
-            Catch ex As Exception
-            End Try
 
-            Dim plattformType = "windows 10"
-            If OS.IsWindowsServer() Then
-                plattformType = "windows server"
-            End If
+            helper.RunLogic(logFile, downloadDir)
 
-            Dim architecture = "x64"
-            If Not System.Environment.Is64BitOperatingSystem Then
-                architecture = "x86"
-            End If
-
-            Dim versionFile As String = IO.Path.Combine(downloadDir, My.Application.Info.Version.ToString)
-            If Not IO.File.Exists(versionFile) Then
-                For Each file As String In IO.Directory.GetFiles(downloadDir)
-                    IO.File.Delete(file)
-                Next
-                IO.File.WriteAllText(versionFile, Environment.OSVersion.VersionString)
-            Else
-                Dim versionNumber As String = IO.File.ReadAllText(versionFile)
-                If Not versionNumber.Equals(Environment.OSVersion.VersionString) Then
-                    For Each file As String In IO.Directory.GetFiles(downloadDir)
-                        IO.File.Delete(file)
-                    Next
-                End If
-                IO.File.WriteAllText(versionFile, Environment.OSVersion.VersionString)
-            End If
-
-
-
-            If helper.IsAMD Then
-                Console.WriteLine("******RegistryPatch AMD******")
-                Dim result As Integer = SpectrePatcherHelper.StartProcess("cmd.exe", "/c " & IO.Path.Combine(My.Application.Info.DirectoryPath & "\", "SpectreRegistryPatchAMD.cmd"))
-                If result <> 0 Then
-                    helper.LogError("Registry-Patch nicht angebracht")
-                End If
-            Else
-                Console.WriteLine("******RegistryPatch Intel******")
-                Dim result As Integer = SpectrePatcherHelper.StartProcess("cmd.exe", "/c " & IO.Path.Combine(My.Application.Info.DirectoryPath & "\", "SpectreRegistryPatchIntel.cmd"))
-                If result <> 0 Then
-                    helper.LogError("Registry-Patch nicht angebracht")
-                End If
-            End If
-
-
-            Dim availableUpdates As List(Of String) = helper.GetUpdateList
-
-            For Each update As String In availableUpdates
-                Try
-
-
-                    Console.WriteLine("******Prüfe Update: " & update & "******")
-                    Console.WriteLine()
-
-
-                    Dim availableDownloads As Dictionary(Of String, String) = helper.SearchDownloadsForUpdate(update)
-                    For Each download As KeyValuePair(Of String, String) In availableDownloads
-                        Try
-                            Dim linkText As String = download.Value.ToLowerInvariant
-
-                            If linkText.Contains(plattformType) AndAlso linkText.Contains(architecture) Then
-                                Console.WriteLine("*Treffer gefunden: " & download.Value)
-                                Console.WriteLine("**GUID:" & download.Key)
-                                Console.WriteLine("**Rufe Download-Link ab")
-                                Dim downloadLink As String = helper.GetDownloadLink(download.Key)
-                                If Not String.IsNullOrEmpty(downloadLink) Then
-                                    Console.WriteLine("***Download-Link:" & downloadLink)
-
-
-
-                                    Console.WriteLine("**Lade herunter")
-                                    Dim destFile As String = helper.DownloadFile(downloadDir, downloadLink)
-
-
-                                    If Not String.IsNullOrEmpty(destFile) Then
-                                        Try
-                                            Console.WriteLine("**Installiere")
-
-
-                                            Dim exitCode As Integer = SpectrePatcherHelper.StartProcess("wusa.exe", destFile & " /quiet /norestart")
-                                            If exitCode <> 0 Then
-                                                If exitCode = -2145124329 Then
-                                                    'Update trifft auf System nicht zu.
-                                                ElseIf exitCode = 2359302 Then
-                                                    'Update bereits installiert.
-                                                ElseIf exitCode = 3010 Then
-                                                    'Neustart erforderlich
-                                                    Console.WriteLine("**Neustart erforderlich. Patchvorgang unvollständig")
-                                                    doReboot = True
-                                                    Exit For
-                                                Else
-                                                    Throw New Exception(String.Format("Installation mit {0} fehlgeschlagen", exitCode))
-
-                                                End If
-                                            End If
-
-                                            Console.WriteLine("**Installiert")
-
-                                        Catch ex As Exception
-                                            If IO.File.Exists(destFile) Then
-                                                IO.File.Delete(destFile)
-                                            End If
-
-                                            Throw ex
-                                        End Try
-                                    End If
-
-                                End If
-
-
-                                Console.WriteLine()
-                            End If
-                        Catch ex As Exception
-
-                            helper.LogError(ex)
-                        End Try
-                    Next
-                Catch ex As Exception
-                    helper.LogError(ex)
-                End Try
-
-            Next
         Catch ex As Exception
             helper.LogError(ex)
+        Finally
+            helper.PrintErrors(logFile)
         End Try
-
-        System.IO.File.AppendAllLines(logFile, helper.ErrorLog)
-        Console.WriteLine("Fertig")
-        If helper.ErrorLog.Count > 0 Then
-            Environment.ExitCode = -1
-            Console.WriteLine("Fehler: ")
-            For Each errorStr As String In helper.ErrorLog
-                Console.WriteLine(errorStr)
-            Next
-        Else
-            Environment.ExitCode = 0
-        End If
-
-
-        If doReboot AndAlso System.Configuration.ConfigurationManager.AppSettings("reboot").ToLowerInvariant.Equals("true") Then
-
-            Reboot()
-        End If
         Environment.Exit(Environment.ExitCode)
+
     End Sub
 
 
 
-    Private Sub Reboot()
-        SpectrePatcherHelper.StartProcess("cmd", "/C shutdown -f -r -t 5")
-    End Sub
 
 End Module
--- Sourcecode/SpectrePatcherLib/SpectrePatcherHelper.vb
+++ Sourcecode/SpectrePatcherLib/SpectrePatcherHelper.vb
@@ -13,21 +13,89 @@
 'GNU General Public License For more details.
 
 'You should have received a copy Of the GNU General Public License
-'along with SpectrePatcher. If Not, see < http: //www.gnu.org/licenses/>.
+'along with SpectrePatcher. If Not, see < http://www.gnu.org/licenses/>.
 Imports System.Management
 Imports System.Net
 Imports System.Text.RegularExpressions
 Imports System.Xml
 
 Public Class SpectrePatcherHelper
-    Public ReadOnly Property ErrorLog As New List(Of String)
+    Private ReadOnly Property ErrorLog As New List(Of String)
 
-    Public Sub LogError(msg As String)
+    Private Sub LogError(msg As String)
         _ErrorLog.Add(msg)
     End Sub
     Public Sub LogError(ex As Exception)
         _ErrorLog.Add(ex.Message)
     End Sub
+
+
+    Public Sub RunLogic(logFile As String, downloadDir As String)
+
+        Dim doReboot As Boolean = False
+        Try
+
+            InitializeDownloadFolder(downloadDir)
+
+            Dim plattformType = "windows 10"
+            If OS.IsWindowsServer() Then
+                plattformType = "windows server"
+            End If
+
+            Dim architecture = "x64"
+            If Not System.Environment.Is64BitOperatingSystem Then
+                architecture = "x86"
+            End If
+
+            ApplyRegistryPatches()
+
+            Dim availableUpdates As List(Of String) = GetUpdateList()
+
+            For Each update As String In availableUpdates
+                Try
+
+                    Console.WriteLine("******Prüfe Update: " & update & "******")
+                    Console.WriteLine()
+
+                    Dim availableDownloads As Dictionary(Of String, String) = SearchDownloadsForUpdate(update)
+
+                    For Each download As KeyValuePair(Of String, String) In availableDownloads
+                        Try
+                            Dim linkText As String = download.Value.ToLowerInvariant
+
+                            If linkText.Contains(plattformType) AndAlso linkText.Contains(architecture) Then
+                                Console.WriteLine("*Treffer gefunden: " & download.Value)
+                                Console.WriteLine("**GUID:" & download.Key)
+                                Console.WriteLine("**Rufe Download-Link ab")
+                                doReboot = Not DownloadAndInstallPatch(download.Key, downloadDir)
+                                If doReboot Then
+                                    Exit For
+                                End If
+                                Console.WriteLine()
+                            End If
+                        Catch ex As Exception
+
+                            LogError(ex)
+                        End Try
+                    Next
+                    If doReboot Then
+                        Exit For
+                    End If
+                Catch ex As Exception
+                    LogError(ex)
+                End Try
+            Next
+        Catch ex As Exception
+            LogError(ex)
+        End Try
+
+        If doReboot AndAlso System.Configuration.ConfigurationManager.AppSettings("reboot").ToLowerInvariant.Equals("true") Then
+            Reboot(logFile)
+        End If
+
+    End Sub
+
+
 
     Public Shared Function StartProcess(file As String, param As String) As Integer
         Using p As New Process
@@ -51,7 +119,7 @@ Public Class SpectrePatcherHelper
 
     End Function
 
-    Public Function IsAMD() As Boolean
+    Private Function IsAMD() As Boolean
         Using mc As ManagementClass = New ManagementClass("Win32_Processor")
             Using moc As ManagementObjectCollection = mc.GetInstances()
 
@@ -68,15 +136,17 @@ Public Class SpectrePatcherHelper
         End Using
     End Function
 
-    Public Function GetUpdateList() As List(Of String)
+    Private Function GetUpdateList() As List(Of String)
         Dim retVal As New List(Of String)
+
         Dim rssDoc = New XmlDocument()
         rssDoc.Load("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/6ae59d69-36fc-8e4d-23dd-631d98bf74a9/rss")
+
         Dim updateList As XmlNodeList = rssDoc.SelectNodes("//rss/channel/item")
         If updateList.Count = 0 Then
             LogError("Keine Updates gefunden.")
-
         End If
+
         For Each update As XmlNode In updateList
             Dim title As XmlNode = update.SelectSingleNode("title")
             If title IsNot Nothing AndAlso Not String.IsNullOrEmpty(title.InnerText) Then
@@ -92,7 +162,7 @@ Public Class SpectrePatcherHelper
     End Function
 
 
-    Public Function SearchDownloadsForUpdate(update As String) As Dictionary(Of String, String)
+    Private Function SearchDownloadsForUpdate(update As String) As Dictionary(Of String, String)
         Dim retVal As New Dictionary(Of String, String)
 
         Dim kbNumberMatch As Match = Regex.Match(update, "(?<number>KB[0-9]{4,})", RegexOptions.IgnoreCase Or RegexOptions.Singleline Or RegexOptions.IgnorePatternWhitespace Or RegexOptions.ExplicitCapture)
@@ -100,10 +170,12 @@ Public Class SpectrePatcherHelper
         If kbNumberMatch IsNot Nothing AndAlso kbNumberMatch.Success AndAlso kbNumberMatch.Groups("number") IsNot Nothing AndAlso Not String.IsNullOrEmpty(kbNumberMatch.Groups("number").Value) Then
 
             Dim kbNumberString As String = kbNumberMatch.Groups("number").Value
+
             Dim searchResult As String = ""
             Using updateSearchClient As New WebClient()
-                searchResult = updateSearchClient.DownloadString("https://www.catalog.update.microsoft.com/Search.aspx?q=" & kbNumberString)
+                searchResult = updateSearchClient.DownloadString("https://www.catalog.update.microsoft.com/Search.aspx?q=" & System.Web.HttpUtility.UrlEncode(kbNumberString))
             End Using
+
             Dim downloadLinksMatches As MatchCollection = Regex.Matches(searchResult, "<a\s+(?<linkAttributes>[^>]+)>(?<linkText>.+?)<\/a>", RegexOptions.IgnoreCase Or RegexOptions.Singleline Or RegexOptions.IgnorePatternWhitespace Or RegexOptions.ExplicitCapture)
             If downloadLinksMatches.Count = 0 Then
                 LogError("Es konnte zu dem Update keine Downloads bestimmt werden. F1")
@@ -111,10 +183,11 @@ Public Class SpectrePatcherHelper
 
             For Each downloadLinksMatch As Match In downloadLinksMatches
                 If downloadLinksMatch.Success AndAlso downloadLinksMatch.Groups("linkAttributes") IsNot Nothing AndAlso downloadLinksMatch.Groups("linkText") IsNot Nothing Then
-                    Dim linkText As String = downloadLinksMatch.Groups("linkText").Value.ToLowerInvariant
-                    Dim linkAttributes As String = downloadLinksMatch.Groups("linkAttributes").Value
 
+                    Dim linkText As String = downloadLinksMatch.Groups("linkText").Value.ToLowerInvariant
                     linkText = linkText.Replace(vbCr, "").Replace(vbLf, "")
+
+                    Dim linkAttributes As String = downloadLinksMatch.Groups("linkAttributes").Value
 
                     If linkText.ToLowerInvariant.Contains(kbNumberString.ToLowerInvariant) Then
                         Dim idMatch As Match = Regex.Match(linkAttributes, "id\=\'(?<id>[^\']+)\'", RegexOptions.IgnoreCase Or RegexOptions.Singleline Or RegexOptions.IgnorePatternWhitespace Or RegexOptions.ExplicitCapture)
@@ -124,7 +197,6 @@ Public Class SpectrePatcherHelper
                                 patchGuid = patchGuid.Replace("_link", "")
                                 retVal.Add(patchGuid, linkText)
                             Else
-
                                 LogError("Es konnte zu dem Update keine Downloads bestimmt werden. F2")
                             End If
                         Else
@@ -144,7 +216,7 @@ Public Class SpectrePatcherHelper
         Return retVal
     End Function
 
-    Public Function GetDownloadLink(patchGuid As String) As String
+    Private Function GetDownloadLink(patchGuid As String) As String
         Dim responseBody As String = ""
         Using client As New Net.WebClient
             Dim reqParm As New Specialized.NameValueCollection
@@ -169,15 +241,17 @@ Public Class SpectrePatcherHelper
     End Function
 
 
-    Public Function DownloadFile(destPath As String, downloadLink As String) As String
+    Private Function DownloadFile(destPath As String, downloadLink As String) As String
         Dim downloadLinkParts As String() = downloadLink.Split("/"c)
-        Dim destFile As String = IO.Path.Combine(destPath, downloadLinkParts(downloadLinkParts.Length - 1))
+        Dim destFileName = New IO.FileInfo(downloadLinkParts(downloadLinkParts.Length - 1))
+        Dim destFile As String = IO.Path.Combine(destPath, destFileName.Name)
         If IO.File.Exists(destFile) Then
             Console.WriteLine("**Datei bereits heruntergeladen")
             Return Nothing
         Else
             Dim checkSum As String = GetCheckSum(downloadLink)
             If (String.IsNullOrEmpty(checkSum)) Then
+                LogError("**Keine Checksumme")
                 Return Nothing
             End If
 
@@ -198,7 +272,7 @@ Public Class SpectrePatcherHelper
 
 
 
-    Public Function GetCheckSum(url As String) As String
+    Private Function GetCheckSum(url As String) As String
         Dim checkSumMatch As Match = Regex.Match(url, "_(?<checksum>[^.]+)\.", RegexOptions.IgnoreCase Or RegexOptions.Singleline Or RegexOptions.IgnorePatternWhitespace Or RegexOptions.ExplicitCapture)
         If checkSumMatch.Success AndAlso checkSumMatch.Groups("checksum") IsNot Nothing AndAlso Not String.IsNullOrEmpty(checkSumMatch.Groups("checksum").Value) Then
             Return checkSumMatch.Groups("checksum").Value
@@ -208,7 +282,7 @@ Public Class SpectrePatcherHelper
         End If
     End Function
 
-    Public Function ValidateCheckSum(refCheckSum As String, file As String) As Boolean
+    Private Function ValidateCheckSum(refCheckSum As String, file As String) As Boolean
         Dim checkSum As String = Nothing
         Using fileStream As IO.FileStream = IO.File.OpenRead(file)
             Using sha1 As System.Security.Cryptography.SHA1 = System.Security.Cryptography.SHA1.Create()
@@ -217,4 +291,113 @@ Public Class SpectrePatcherHelper
         End Using
         Return refCheckSum.ToLowerInvariant().Equals(checkSum.ToLowerInvariant())
     End Function
+
+    Private Sub InitializeDownloadFolder(downloadDir As String)
+
+        Try
+            If Not IO.Directory.Exists(downloadDir) Then
+                IO.Directory.CreateDirectory(downloadDir)
+            End If
+        Catch ex As Exception
+        End Try
+
+        Dim versionFile As String = IO.Path.Combine(downloadDir, My.Application.Info.Version.ToString)
+        If Not IO.File.Exists(versionFile) Then
+            For Each file As String In IO.Directory.GetFiles(downloadDir)
+                IO.File.Delete(file)
+            Next
+            IO.File.WriteAllText(versionFile, Environment.OSVersion.VersionString)
+        Else
+            Dim versionNumber As String = IO.File.ReadAllText(versionFile)
+            If Not versionNumber.Equals(Environment.OSVersion.VersionString) Then
+                For Each file As String In IO.Directory.GetFiles(downloadDir)
+                    IO.File.Delete(file)
+                Next
+            End If
+            IO.File.WriteAllText(versionFile, Environment.OSVersion.VersionString)
+        End If
+
+    End Sub
+
+    Private Sub ApplyRegistryPatches()
+        If IsAMD() Then
+            Console.WriteLine("******RegistryPatch AMD******")
+            Dim result As Integer = SpectrePatcherHelper.StartProcess("cmd.exe", "/c " & IO.Path.Combine(My.Application.Info.DirectoryPath & "\", "SpectreRegistryPatchAMD.cmd"))
+            If result <> 0 Then
+                LogError("Registry-Patch nicht angebracht")
+            End If
+        Else
+            Console.WriteLine("******RegistryPatch Intel******")
+            Dim result As Integer = SpectrePatcherHelper.StartProcess("cmd.exe", "/c " & IO.Path.Combine(My.Application.Info.DirectoryPath & "\", "SpectreRegistryPatchIntel.cmd"))
+            If result <> 0 Then
+                LogError("Registry-Patch nicht angebracht")
+            End If
+        End If
+
+    End Sub
+    Private Function DownloadAndInstallPatch(downloadId As String, downloadDir As String) As Boolean
+        Dim downloadLink As String = GetDownloadLink(downloadId)
+
+        If Not String.IsNullOrEmpty(downloadLink) Then
+            Console.WriteLine("***Download-Link:" & downloadLink)
+
+            Console.WriteLine("**Lade herunter")
+            Dim destFile As String = DownloadFile(downloadDir, downloadLink)
+
+            If Not String.IsNullOrEmpty(destFile) Then
+                Try
+                    Console.WriteLine("**Installiere")
+
+                    Dim exitCode As Integer = SpectrePatcherHelper.StartProcess("wusa.exe", destFile & " /quiet /norestart")
+                    If exitCode <> 0 Then
+                        If exitCode = -2145124329 Then
+                            'Update trifft auf System nicht zu.
+                        ElseIf exitCode = 2359302 Then
+                            'Update bereits installiert.
+                        ElseIf exitCode = 3010 Then
+                            'Neustart erforderlich
+                            Console.WriteLine("**Neustart erforderlich. Patchvorgang unvollständig")
+                            Return False
+
+                        Else
+                            Throw New Exception(String.Format("Installation mit {0} fehlgeschlagen", exitCode))
+
+                        End If
+                    End If
+
+                    Console.WriteLine("**Installiert")
+
+                Catch ex As Exception
+                    If IO.File.Exists(destFile) Then
+                        IO.File.Delete(destFile)
+                    End If
+
+                    Throw ex
+                End Try
+            End If
+
+        End If
+        Return True
+    End Function
+
+
+    Public Sub PrintErrors(logFile As String)
+        System.IO.File.AppendAllLines(logFile, ErrorLog)
+        Console.WriteLine("Fertig")
+        If ErrorLog.Count > 0 Then
+            Environment.ExitCode = -1
+            Console.WriteLine("Fehler: ")
+            For Each errorStr As String In ErrorLog
+                Console.WriteLine(errorStr)
+            Next
+        Else
+            Environment.ExitCode = 0
+        End If
+        ErrorLog.Clear()
+    End Sub
+
+    Private Sub Reboot(logFile As String)
+        PrintErrors(logFile)
+        StartProcess("cmd", "/C shutdown -f -r -t 5")
+    End Sub
 End Class
